3b6c8045510e32de9027b2e94d53c45967605d45
Muhammad Abubakar <98586854+abbiimughal@users.noreply.github.com>
2023-05-03 19:32:41 +0500

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
         <button
           type='button'
           onClick={() =>
-            window.open("https://github.com/TidbitsJS/Summize", "_blank")
+            window.open("", "_blank")
           }
           className='black_btn'
         >
